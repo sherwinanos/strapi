ad9b794aac61ba6fd114070724e208a6e563d48c
Sherwin Anos <sherwin.anos@gmail.com>
2024-09-17 19:51:38 +0800

    Update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,20 @@
 import { Button } from "@/components/ui/button"
 
-export default function Home() {
+async function getStrapiData(path: string) {
+  const baseUrl = "http://localhost:1337"
+  try {
+    const response = await fetch(baseUrl + path)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export default async function Home() {
+  const strapiData = await getStrapiData("/api/home-page")
+  console.log('Strapi Data:', strapiData)
+
   return (
     <main className="container mx-auto py-6">
       <Button>Learn more</Button>
